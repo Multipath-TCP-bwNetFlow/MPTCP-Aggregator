4f1183f79b09ff1fd25ae27b63890bb78d9893bf
jannikseemann <jannmann1@web.de>
2021-01-03 22:39:31 +0100

    fix typo

--- src/main/java/bwnetflow/mptcp/aggregator/InternalTopic.java
+++ src/main/java/bwnetflow/mptcp/aggregator/InternalTopic.java
@@ -1,5 +1,5 @@
 package bwnetflow.mptcp.aggregator;
 
 public class InternalTopic {
-    public static final String AGGREGATOR_OUTPUT = "mptcp-benetflow-aggregator-output";
+    public static final String AGGREGATOR_OUTPUT = "mptcp-bwnetflow-aggregator-output";
 }
